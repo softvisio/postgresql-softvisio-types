bbb339b934a99da6cf8de0e98c03125c35edf8f7
zdm <zdm@softvisio.net>
2026-01-10 12:55:35 +0200

    feat: add lo_size() function

--- softvisio_types--1.1.6--1.2.0.sql
+++ softvisio_types--1.1.6--1.2.0.sql
@@ -3,26 +3,26 @@
 \echo 'ALTER EXTENSION softvisio_types UPDATE;'
 \echo \quit
 
-CREATE OR REPLACE FUNCTION lo_size ( oid ) RETURNS integer AS $$
+CREATE OR REPLACE FUNCTION lo_size ( oid ) RETURNS int53 STABLE AS $$
 DECLARE
-    fd integer;
-    sz integer;
+    fd int4;
+    size int53;
 BEGIN
     -- open the large object in read mode (mode 262144)
-    fd = lo_open($1, 262144);
+    fd = lo_open( $1, 262144 );
 
     IF ( fd < 0 ) THEN
         RAISE EXCEPTION 'Failed to open large object %', $1;
     END IF;
 
     -- seek to the end of the object (offset 0, whence 2) and get the position
-    sz = lo_lseek( fd, 0, 2 );
+    size = lo_lseek( fd, 0, 2 );
 
     -- close the large object
     IF ( lo_close( fd ) != 0 ) THEN
         RAISE EXCEPTION 'Failed to close large object %', $1;
     END IF;
 
-    RETURN sz;
+    RETURN size;
 END;
 $$ LANGUAGE plpgsql;
