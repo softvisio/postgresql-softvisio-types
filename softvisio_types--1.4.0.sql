5ca259b49544f78ede6e83392577395482f8ff28
zdm <zdm@softvisio.net>
2026-09-14 10:06:41 +0300

    fix: fix types patch

--- softvisio_types--1.4.0.sql
+++ softvisio_types--1.4.0.sql
@@ -3,32 +3,6 @@
 \echo 'ALTER EXTENSION softvisio_types UPDATE;'
 \echo \quit
 
-DROP FUNCTION IF EXISTS lo_size ( oid );
-
-CREATE OR REPLACE FUNCTION lo_size ( lo_oid oid ) RETURNS int53 STABLE AS $$
-DECLARE
-    v_fd int4;
-    v_size int53;
-BEGIN
-    -- open the large object in read mode (mode 262144)
-    v_fd = lo_open( lo_size.lo_oid, 262144 );
-
-    IF ( v_fd < 0 ) THEN
-        RAISE EXCEPTION 'Failed to open large object %', lo_size.lo_oid;
-    END IF;
-
-    -- seek to the end of the object (offset 0, whence 2) and get the position
-    v_size = lo_lseek( v_fd, 0, 2 );
-
-    -- close the large object
-    IF ( lo_close( v_fd ) != 0 ) THEN
-        RAISE EXCEPTION 'Failed to close large object %', lo_size.lo_oid;
-    END IF;
-
-    RETURN v_size;
-END;
-$$ LANGUAGE plpgsql;
-
 DO $$ BEGIN
 
     IF to_regtype( 'int53' ) IS NULL THEN
@@ -100,3 +74,29 @@ DO $$ BEGIN
     END IF;
 
 END $$;
+
+DROP FUNCTION IF EXISTS lo_size ( oid );
+
+CREATE OR REPLACE FUNCTION lo_size ( lo_oid oid ) RETURNS int53 STABLE AS $$
+DECLARE
+    v_fd int4;
+    v_size int53;
+BEGIN
+    -- open the large object in read mode (mode 262144)
+    v_fd = lo_open( lo_size.lo_oid, 262144 );
+
+    IF ( v_fd < 0 ) THEN
+        RAISE EXCEPTION 'Failed to open large object %', lo_size.lo_oid;
+    END IF;
+
+    -- seek to the end of the object (offset 0, whence 2) and get the position
+    v_size = lo_lseek( v_fd, 0, 2 );
+
+    -- close the large object
+    IF ( lo_close( v_fd ) != 0 ) THEN
+        RAISE EXCEPTION 'Failed to close large object %', lo_size.lo_oid;
+    END IF;
+
+    RETURN v_size;
+END;
+$$ LANGUAGE plpgsql;
